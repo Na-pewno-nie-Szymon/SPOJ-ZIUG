10dfbf2dcebd14cb8b3bf56c5b379a32ab562bc4
Na-pewno-nie-Szymon <119611945+Na-pewno-nie-Szymon@users.noreply.github.com>
2023-10-08 12:32:50 +0200

    Time limit exceeded
    Time limit:       1s, for now its compiling in around 3.26s
    Source limit:	50000B
    Memory limit:	1536M

---
+++ Dodawanie.py
@@ -0,0 +1,27 @@
+def aMozeTo(liczba):
+    if liczba % 2 == 0 or liczba % 5 == 0:	# wykluczenie warunków niemożliwyych
+        return 'NIE'  
+      
+    l = liczba
+    iloscJeden = 0
+    
+    while liczba > 0:	# główny algorytm liczenia długości liczby złożonej z samych jedynek 
+        if liczba % 10 == 1:
+            liczba = liczba // 10
+            iloscJeden += 1
+        else:
+            liczba += l 
+
+    return iloscJeden
+
+def main():
+    liczby = []
+    t = int(input())	# ilosc testow <= 10000
+    for i in range(t):
+        liczby.append(int(input()))	# liczba do sprawdzenia <1;10000>
+
+    for i in range(t):	# wypisywanie wyników
+        print(aMozeTo(liczba=liczby[i]))
+
+if __name__=='__main__':
+    main()
